56dd026f7523c76678a335cd6b0ba53f3567a759
Dmitriy Berejnoi <dberejno@gmail.com>
2018-10-25 15:16:48 -0600

    routing started

---
+++ src/components/navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import Movies from "./movies";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand mb-0 h1" href="#">
+        Vidly
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="topNav">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
